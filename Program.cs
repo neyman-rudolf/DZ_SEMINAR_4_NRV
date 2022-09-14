cb9624e19e2629eb56d0eb9a8db5c2384eb06482
Rudolf Neyman <neyman.rudolf@yandex.ru>
2022-09-14 21:52:36 +0300

    Решение задачи №27

--- Program.cs
+++ Program.cs
@@ -18,3 +18,31 @@
 // }
 //   int Constr = Construction(numberA, numberB);
 //   Console.WriteLine("Ответ: " + Constr);
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Console.Write("Введите число N: ");
+// int numberN = Convert.ToInt32(Console.ReadLine());
+
+//   int SumNumber(int numberN)
+//   {
+    
+//     int count = Convert.ToString(numberN).Length;
+//     int category = 0;
+//     int result = 0;
+
+//     for (int i = 0; i < count; i++)
+//     {
+//       category = numberN - numberN % 10;
+//       result = result + (numberN - category);
+//       numberN = numberN / 10;
+//     }
+//    return result;
+//   }
+
+// int sumNumber = SumNumber(numberN);
+// Console.WriteLine("Сумма цифр в числе: " + sumNumber);
