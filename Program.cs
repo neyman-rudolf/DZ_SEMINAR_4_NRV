5f76761651173d7b07e7df924f4bfd701cbd4d91
Rudolf Neyman <neyman.rudolf@yandex.ru>
2022-09-14 22:01:15 +0300

    Решение задачи №29

--- Program.cs
+++ Program.cs
@@ -46,3 +46,20 @@
 
 // int sumNumber = SumNumber(numberN);
 // Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+//  int[] GetBinaryArray(int size)
+// {
+//     int[] result = new int[size]; 
+//     for (int i = 0; i < result.Length; i++) 
+//     {
+//         result[i] = new Random().Next(0,100); 
+//     }
+//     return result;
+// }
+// int[] testArray = GetBinaryArray(8);
+// Console.WriteLine($"Массив: [ {String.Join("; ",testArray)} ]");
